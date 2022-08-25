5a94847797f2c6cbbc564fdfa2e26cc4c9c2c7a4
Michix <em.telenga24@gmail.com>
2022-08-25 21:59:24 +0200

    Implement Version in server

---
+++ server/src/main/java/me/glicz/airflow/server/types/AirVersion.java
@@ -0,0 +1,26 @@
+package me.glicz.airflow.server.types;
+
+import lombok.Getter;
+import me.glicz.airflow.types.Version;
+
+public class AirVersion implements Version {
+
+    @Getter
+    private final int major, minor, patch, protocolVersion;
+
+    private AirVersion(int major, int minor, int patch, int protocolVersion) {
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
+        this.protocolVersion = protocolVersion;
+    }
+
+    public static AirVersion from(int major, int minor, int patch, int protocolVersion) {
+        return new AirVersion(major, minor, patch, protocolVersion);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("v%s.%s.%s (%s)", major, minor, patch, protocolVersion);
+    }
+}
