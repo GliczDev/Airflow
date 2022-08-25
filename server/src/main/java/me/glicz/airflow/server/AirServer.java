48896a60ae5fbdc29ff2731f60e91b33040a705e
Michix <em.telenga24@gmail.com>
2022-08-25 22:02:55 +0200

    Implement Server changes in server module

--- server/src/main/java/me/glicz/airflow/server/AirServer.java
+++ server/src/main/java/me/glicz/airflow/server/AirServer.java
@@ -1,11 +1,22 @@
 package me.glicz.airflow.server;
 
+import lombok.Getter;
+import me.glicz.airflow.Airflow;
 import me.glicz.airflow.Server;
+import me.glicz.airflow.server.types.AirVersion;
+import me.glicz.airflow.types.Version;
+
 import java.util.logging.Logger;
 
 public class AirServer implements Server {
 
+    @Getter
+    private final String name = "Airflow";
+    @Getter
+    private final Version version = AirVersion.from(1, 19, 2, 760);
+
     public static void main(String[] args) {
-        Logger.getLogger("Airflow").info("In development.");
+        Airflow.setServer(new AirServer());
+        Logger.getLogger("Airflow").info("In development. Minecraft Version: " + Airflow.getServer().getVersion().toString());
     }
 }
